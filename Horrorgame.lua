edd98ee1c03ac0e24a08f58a1dd0dc69914d108f
Ben <110933610+TheKnownPerson@users.noreply.github.com>
2025-10-10 12:35:13 +0200

    Horrorgame.lua aktualisieren

--- Horrorgame.lua
+++ Horrorgame.lua
@@ -1,13 +1,23 @@
-local flowerrig = game:GetService("Workspace"):WaitForChild("Flower")
+local flowerrig = game:GetService("ReplicatedStorage"):WaitForChild("Flower")
 local Player = game:GetService("Players").LocalPlayer
 local GUI = Player:WaitForChild("PlayerGui")
 local entity = GUI:WaitForChild("ScreenGui"):WaitForChild("entity"):WaitForChild("done")
 local behind = entity:WaitForChild("look behind you")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local function randomchance()
+	local chance = math.random(1,4)
+	return chance
+end
 
 behind.Changed:Connect(function(prop)
 	if prop == "Visible" then
-		if behind.Visible = true then
-			
+		if behind.Visible then
+			local chance = randomchance()
+			if chance == 1 then
+				Flower.Parent = game:GetService("Workspace")
+			task.wait(math.random(3, 6)) 
+			end
 		end
 	end
 end)
